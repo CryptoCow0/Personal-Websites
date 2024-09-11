a39f5b77d5c09d482ec74d8770234c430a23f179
Miguel Wills <miguelcow1@gmail.com>
2024-09-10 18:47:24 -0600

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import AboutMe from './AboutMe';
 import OverView from './OverView';
 import Projects from './Projects';
 import './App.css'; // Import any global CSS if necessary
-import ReactDOM from 'react-dom/client';
-import { createRoot } from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.css';
+
 
 const App: React.FC = () => {
   return (
